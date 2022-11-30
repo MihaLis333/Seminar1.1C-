49db90059b7025da0e206106fdf3b95f0b85ee12
MihaLis333 <m190333@ya.ru>
2022-11-30 13:44:20 +0300

    для перезагрузки VSC

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 5;, b = 3;
+
+
+
+
+Console.WriteLine("my number is " + a + "and" + b + "and it is good!");
+
