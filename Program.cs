0fe4bcffb784dc1800d7d305b93dd1b3480f311d
MihaLis333 <m190333@ya.ru>
2022-12-01 13:27:26 +0300

    добавление вариантов задач

--- Program.cs
+++ Program.cs
@@ -1,4 +1,48 @@
-﻿Console.Write("input a number: ");
+﻿
+/*
+Console.Write("input a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad = num2 * num2;
+if(num1 == quad)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
+
+// Задача 2. Напишите задачу котрая на вход принимает одно число (N), а на выходе показывает все целые числа от (-N) до (N).
+
+/*
+Console.Write("Input an integer number: ");
 int num = Convert.ToInt32(Console.ReadLine());
 
-"25"
+int current = -num;
+
+while (current <= num)
+{
+    Console.Write(current + " ");
+    current++;
+}
+*/
+
+// Задача 3. 
+/*
+12345 / 10 =1234
+12345 / 100 = 123
+12345 / 1000 = 12
+12345 / 10000 = 1
+
+12345 % 10 =5
+12345 % 100 = 45
+12345 % 1000 = 345
+12345 % 10000 = 2345
+12345 % 100000 = 12345
+*/
+
+
